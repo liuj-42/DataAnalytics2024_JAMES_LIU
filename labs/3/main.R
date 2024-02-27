3ddfd1372dcbfa7692bae5b031977fd69fbc4fb8
James Liu <liuxjames88@gmail.com>
2024-02-27 11:44:20 -0500

    lab 3

--- labs/3/main.R
+++ labs/3/main.R
@@ -39,7 +39,11 @@ plot(colMeans(data_Matrix_Ordered), xlab="Column", ylab="Column Mean", pch=19)
 library(tidyverse) #ggplot and dplyr
 library(class) #knn
 
-abalone <- read.csv("./abalone.csv")
+abalone <- read.csv("./abalone.csv", stringsAsFactors = TRUE)
+
+abalone$Sex <- as.integer(abalone$Sex)
+
+# data$iGender = as.integer(data$Gender)
 
 ggplot(data=abalone,aes(x=Sex,fill=Sex))+geom_bar()
 
@@ -92,3 +96,42 @@ classif <- knn(train, test, crings, k=6)  # (main.R#90): NA/NaN/Inf in foreign f
 classif
 
 attributes(.Last.value)
+
+# ======================================================================================
+# =============================  Exercise 3  ===========================================
+# ======================================================================================
+
+# Create a new data frame and remove the fifth
+# column
+
+df <- iris[,-5] # without 5th col
+df
+
+# Apply kmeans (you choose k) with 1000
+# iterations
+
+set.seed(101)
+help(kmeans)
+irisClusters <- kmeans(df, 3, iter.max = 1000)
+
+clusplot(iris,irisClusters$cluster, color = TRUE, shade = TRUE, labels = 0, lines = 0)
+par(pty="m")
+table(iris[,5], irisClusters$cluster)
+
+
+# ======================================================================================
+# ===============================  Titanic  ============================================
+# ======================================================================================
+library(titanic)
+library(tidyverse)
+head(titanic_train)
+titanic <- titanic_train
+str(titanic_train)
+summary(titanic_train)
+
+help(rpart)
+help(ctree)
+help(hclust)
+
+any(is.na(titanic_train))
+titanic <- na.omit(titanic) # remove columns with na values
