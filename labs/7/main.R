0a0c8b9b7c8f3978a875fb24b5008936d07386a1
James Liu <liuxjames88@gmail.com>
2024-03-29 11:30:06 -0400

    titanic random forest

--- labs/7/main.R
+++ labs/7/main.R
@@ -64,8 +64,37 @@ pve <- pr.var/sum(pr.var)
 pve
 
 data(Titanic)
+library(titanic)
+library(randomForest)
 
 # randomforest
 
 library(tidyverse)
+library(rpart)
+
+head(titanic_train)
+
+summary(titanic_train)
+summary(titanic_train$Survived)
+
+# fit <- rpart(Survived~., data=titanic_train[!is.na(titanic_train$Age),], method="anova")
+
+fit <- randomForest(Survived~., data=titanic_train[!is.na(titanic_train$Age),])
+# ntree restricts the number of trees generated
+# nodesize reduces the complexity of each tree generated
+# samsize reduces the number of rows sampled
+
+
+Prediction <- predict(fit, titanic_test)
+results <- data.frame(PassengerId = titanic_test$PassengerId, Survived = Prediction)
+
+library(party)
+
+# Convert all character columns to factors
+titanic_train[] <- lapply(titanic_train, function(x) {
+  if (is.character(x)) as.factor(x) else x
+})
+
+# Now you can run the cforest function
+fit2 <- cforest(Survived~., data=titanic_train[!is.na(titanic_train$Age),])
 
